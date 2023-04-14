3a84f3ed5a345beecb765b347b0620975f52c186
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2023-04-14 10:35:15 +0800

    Update StringUtilTest.java

--- test/unit/java/com/articulate/sigma/StringUtilTest.java
+++ test/unit/java/com/articulate/sigma/StringUtilTest.java
@@ -20,7 +20,7 @@ public class StringUtilTest {
     @Test
     public void testFilterHtml() {
 
-        String input = "<ul><li>if for all <a href=\"&term=Entity\">an entity</a> <a href=\"&term=Entity\">the entity</a> is an <a href=\"&term=element\">element</a> of <a href=\"&term=Set\">a  set</a> if and only if <a href=\"&term=Entity\">the entity</a> is an <a href=\"&term=element\">element</a> of <a href=\"&term=Set\">another set</a>,</li><li>then <a href=\"&term=Set\">the set</a> is <a href=\"&term=equal\">equal</a> to <a href=\"&term=Set\">the other set</a></li></ul>";
+        String input = "<><li>if for all <a href=\"&term=Entity\">an entity</a> <a href=\"&term=Entity\">the entity</a> is an <a href=\"&term=element\">element</a> of <a href=\"&term=Set\">a  set</a> if and only if <a href=\"&term=Entity\">the entity</a> is an <a href=\"&term=element\">element</a> of <a href=\"&term=Set\">another set</a>,</li><li>then <a href=\"&term=Set\">the set</a> is <a href=\"&term=equal\">equal</a> to <a href=\"&term=Set\">the other set</a></li></ul>";
         String actual = StringUtil.filterHtml(input);
         String expected = "if for all an entity the entity is an element of a set if and only if the entity is an element of another set, " +
                 "then the set is equal to the other set";
