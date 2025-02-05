0a1435fb61a6e48c00dd2f58098f59c9013fa926
git <tdnorbra@nps.edu>
2025-02-05 13:00:56 -0800

    [Terry N.] formalize test suite launching and report generation

---
+++ test/unit/java/com/articulate/sigma/UnitTestSuite.java
@@ -0,0 +1,18 @@
+package com.articulate.sigma;
+
+import com.articulate.sigma.utils.UnitUtilsTestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This software is released under the GNU Public License
+ * <http://www.gnu.org/copyleft/gpl.html>
+ * @author <a href="mailto:tdnorbra@nps.edu?subject=com.articulate.sigma.UnitTestSuite">Terry Norbraten, NPS MOVES</a>
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    UnitUtilsTestSuite.class
+})
+public class UnitTestSuite {
+
+} // end class file UnitTestSuite.java
